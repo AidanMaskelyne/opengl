cffdad62ff23232ea61a14c2aa1ffa407786b466
Aidan <aidan@muttleyville.org>
2024-03-07 21:46:07 +0000

    Sort out Windows build

--- premake5.lua
+++ premake5.lua
@@ -1,5 +1,6 @@
 workspace "OpenGL"
 	configurations { "Debug", "Release" }
+	architecture "x86_64"
 
 project "OpenGL"
 	kind "ConsoleApp"
@@ -16,13 +17,15 @@ project "OpenGL"
 
 	includedirs {
 		"deps/glad/include",
+		"deps/glfw/include",	-- Only needed for Windows atm
 	}
 
 	filter { "system:linux" }
 		links { "glfw" }
 	filter { "system:windows" }
 		-- TODO: Link pre-built binaries
-		-- TODO: Add GLFW include dirs
+		libdirs { "deps/glfw/lib/windows" }
+		links { "glfw3.lib" }
 	filter {  }
 
 	filter { "configurations:Debug" }
